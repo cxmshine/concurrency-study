b46660cc7bd55ffa38728a4465b4f22d5dc9c5bc
cxmshine <37296677+cxmshine@users.noreply.github.com>
2019-06-24 21:19:31 +0800

    新建一个项目

---
+++ src/test/java/com/mmall/concurrencystudy/ConcurrencyStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrencystudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
