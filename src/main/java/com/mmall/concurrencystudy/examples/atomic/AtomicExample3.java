777e453037e04e975f750faa69acc899f3ff4456
cxmshine <13724591964@163.com>
2019-06-24 23:05:11 +0800

    Atomic相关类的API使用与理解.

---
+++ src/main/java/com/mmall/concurrencystudy/examples/atomic/AtomicExample3.java
@@ -0,0 +1,60 @@
+package com.mmall.concurrencystudy.examples.atomic;
+
+import com.mmall.concurrencystudy.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * jdk8新增的LongAdder,和AtomicLong非常相似.
+ * 实际使用中,如果是高并发场景,则优先使用LongAdder;
+ * 如果是并发度较低的,则使用AtomicLong,因为它更为简单,效率也会高一些.
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample3 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static LongAdder count = new LongAdder();
+
+    // 模拟并发测试
+    public static void main(String[] args) throws Exception{
+        //定义一个线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        //定义计数器闭锁
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i=0;i<clientTotal;i++){
+            executorService.execute(() ->{
+                try {
+                    semaphore.acquire(); // 判断进程是否允许被执行,当达到一定的并发数后,add()有可能被临时阻塞
+                    add();
+                    semaphore.release(); // 执行完以后,将信号量释放
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 每次顺利执行完上面的一次操作(即顺利调用add()方法)后,计数器的值减1
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await(); // 能够保证计数器的值为0时,才执行后面的代码
+        //关闭线程池
+        executorService.shutdown();
+        log.info("count:{}",count);
+    }
+
+    private static void add(){
+        count.increment();
+    }
+}
