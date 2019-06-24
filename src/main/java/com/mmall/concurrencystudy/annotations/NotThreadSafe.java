4e63e3bb3ed3050965e012774f8d79dd2c8193df
cxmshine <13724591964@163.com>
2019-06-24 22:26:42 +0800

    自定义了4个注解;实现模拟并发测试.

---
+++ src/main/java/com/mmall/concurrencystudy/annotations/NotThreadSafe.java
@@ -0,0 +1,14 @@
+package com.mmall.concurrencystudy.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记【线程不安全】的类或写法
+ */
+@Target(ElementType.TYPE)   //该注解作用在类上
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+}
